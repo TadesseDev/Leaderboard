ef3f04ba2c9db6d00cbf971255d71d2c087717dd
Tadesse-Alemayehu <email24t@gmail.com>
2022-04-06 23:25:36 +0300

    Add: spiner

--- src/modules/creatDom.js
+++ src/modules/creatDom.js
@@ -7,9 +7,8 @@ const initialPageContent = `    <div id="page-container">
           Recent scores
           <button type="submit" id="refresh"><span class="count">0 </span><span class="icon"><span></button>
         </h1>
-        <div id="score-list">
+        <div id="score-list" class="loading">
           <ul>
-          <h1> loading user data</h1>
           </ul>
         </div>
       </section>
@@ -21,6 +20,10 @@ const initialPageContent = `    <div id="page-container">
           <button type="submit" class="add-score">Submit</button>
         </form>
       </section>
+      <footer>
+      <h3> Lead Board project <h3>
+      <h5>By,Tadesse Alemayehu </h5>
+      </footer>
     </div>`;
 const createInitialDom = () => {
   document.body.insertAdjacentHTML('beforebegin', initialPageContent);
@@ -39,11 +42,11 @@ const createScoreTile = (score) => {
 
 // once score is ready, update the dom with the given list of scores
 const updateDomWithScore = (scores) => {
-  const scoreListContainer = document.getElementById('score-list').getElementsByTagName('ul')[0];
+  const scoreList = document.getElementById('score-list');
+  const scoreListContainer = scoreList.getElementsByTagName('ul')[0];
   const count = document.getElementsByClassName('count')[0];
   count.innerHTML = scores.length + " ";
-  console.log(count);
-  scoreListContainer.innerHTML = '';
+  scoreList.classList.remove('loading');
   scores.forEach((score) => {
     const newScore = createScoreTile(score);
     scoreListContainer.appendChild(newScore);
