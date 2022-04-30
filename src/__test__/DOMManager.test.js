1e1218fd394696caf928eb17602e8bb4174e9554
Tadesse-Alemayehu <email24t@gmail.com>
2022-04-30 16:28:12 +0300

    Test getFetchUrl and getPostUrl

--- src/__test__/DOMManager.test.js
+++ src/__test__/DOMManager.test.js
@@ -1,9 +1,4 @@
-import { getFetchUrl, getPostUrl, createInitialDom } from '../modules/GLOBALS'
-describe('check post and get urls', () => {
-  test('get POST url passing an id', () => {
-    const temID = 'test/123';
-    const fetchULT = getFetchUrl(temID);
-    const expected = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${temID}/scores/`;
-    expect(fetchULT).toBe(expected);
-  })
-});
+describe('', () => {
+  test('', () => {
+  });
+})
--- src/__test__/GLOBALS.test.js
+++ src/__test__/GLOBALS.test.js
@@ -1,9 +1,12 @@
 import { getFetchUrl, getPostUrl, createInitialDom } from '../modules/GLOBALS'
 describe('check post and get urls', () => {
   test('get POST url passing an id', () => {
-    const temID = 'test/123';
-    const fetchULT = getFetchUrl(temID);
-    const expected = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${temID}/scores/`;
-    expect(fetchULT).toBe(expected);
+    const tempID = 'test/123';
+    const fetchURT = getFetchUrl(tempID);
+    const postURL = getPostUrl(tempID);
+    const getExpected = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${tempID}/scores/`;
+    const postExpected = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${tempID}/scores`;
+    expect(fetchURT).toBe(getExpected);
+    expect(postURL).toBe(postExpected);
   })
 });
