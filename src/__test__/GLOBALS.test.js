57ae31d8d489a41f946f2943e2550c3d41d382b4
Tadesse-Alemayehu <email24t@gmail.com>
2022-04-30 17:45:54 +0300

    install and configure jest-environment-jsdom

--- src/__test__/GLOBALS.test.js
+++ src/__test__/GLOBALS.test.js
@@ -1,4 +1,5 @@
 import { getFetchUrl, getPostUrl, createInitialDom } from '../modules/GLOBALS'
+document.body.innerHTML = '<ul id="to-do-lists"></ul>';
 describe('check post and get urls', () => {
   test('get POST url passing an id', () => {
     const tempID = 'test/123';
@@ -8,5 +9,5 @@ describe('check post and get urls', () => {
     const postExpected = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${tempID}/scores`;
     expect(fetchURT).toBe(getExpected);
     expect(postURL).toBe(postExpected);
-  })
+  });
 });
